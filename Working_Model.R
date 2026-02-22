b16825cb0abeedfdb62babf9bcd75adc8a43f131
Lynette Schwanger <lynette.schwanger@gmail.com>
2026-02-22 13:14:00 -0700

    v9 - wooo plots done

--- Working_Model.R
+++ Working_Model.R
@@ -78,4 +78,72 @@ plot(train_tw4_8$NDVI, train_tw4_8$GPP_NT_VUT_REF,
 #I wonder if it's ok to train on more data than just my 8 dates for raster prediction? 
 #looking closer at simpleworkflow_solution I think so
 #lets start over
+tw4_ndvi <- tw4_hls %>%
+  filter(is_clear == TRUE) %>%
+  mutate(NDVI = (nir - B04) / (nir + B04)) %>%
+  group_by(date) %>%
+  summarise(NDVI = mean(NDVI, na.rm = TRUE), .groups = "drop")
 
+tw4_model_df <- tw4_flux %>%
+  inner_join(tw4_ndvi, by = "date")
+
+tw1_ndvi <- tw1_hls %>%
+  filter(is_clear == TRUE) %>%
+  mutate(NDVI = (nir - B04) / (nir + B04)) %>%
+  group_by(date) %>%
+  summarise(NDVI = mean(NDVI, na.rm = TRUE), .groups = "drop")
+
+tw1_model_df <- tw1_flux %>%
+  inner_join(tw1_ndvi, by = "date")
+
+#ok, now model time fr
+mod <- lm(GPP_NT_VUT_REF ~ NDVI, data = tw4_model_df)
+
+predict_train <- predict(mod, newdata = tw4_model_df)
+
+rmse_train <- sqrt(sum((tw4_model_df$GPP_NT_VUT_REF - predict_train)^2) / length(predict_train))
+rmse_train
+
+par(mar = c(8, 5, 4, 2) + 0.1)
+
+plot(predict_train, tw4_model_df$GPP_NT_VUT_REF,
+     main = "US-Tw4 Training Model Fit: GPP predicted from NDVI",
+     xlab = "Predicted GPP (gC m-2 d-1)",
+     ylab = "Observed GPP (gC m-2 d-1)")
+abline(0, 1, lty = 2)
+
+mtext(side = 3, adj = 0.05, line = -2,
+      paste0("RMSE = ", round(rmse_train, 2)))
+
+caption <- "Figure 3a: Observed vs. predicted daily GPP at US-Tw4 from a linear regression model using NDVI computed from HLS reflectance. RMSE for the training dataset is 3.13 gC m-2 d-1."
+
+mtext(
+  side = 1, adj = 0, line = 6,
+  text = paste(strwrap(caption, width = 130), collapse = "\n"),
+  cex = 0.8
+)
+
+#onto testing
+predict_test <- predict(mod, newdata = tw1_model_df)
+
+rmse_test <- sqrt(sum((tw1_model_df$GPP_NT_VUT_REF - predict_test)^2) / length(predict_test))
+rmse_test
+
+par(mar = c(8, 5, 4, 2) + 0.1)
+
+plot(predict_test, tw1_model_df$GPP_NT_VUT_REF,
+     main = "US-Tw1 Model Test Site: GPP predicted from NDVI",
+     xlab = "Predicted GPP (gC m-2 d-1)",
+     ylab = "Observed GPP (gC m-2 d-1)")
+abline(0, 1, lty = 2)
+
+mtext(side = 3, adj = 0.05, line = -2,
+      paste0("RMSE = ", round(rmse_test, 2)))
+
+caption <- "Figure 3b: Observed vs. predicted daily GPP at test site US-Tw1 from a linear regression model trained at US-Tw4 using NDVI computed from HLS reflectance. RMSE for the training dataset is 2.64 gC m-2 d-1."
+
+mtext(
+  side = 1, adj = 0, line = 6,
+  text = paste(strwrap(caption, width = 130), collapse = "\n"),
+  cex = 0.8
+)
